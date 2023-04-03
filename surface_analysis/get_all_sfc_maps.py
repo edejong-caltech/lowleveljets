6f8b50e90081e86ac11c813f10d5e17b1e604401
Eliot Quon <eliot.quon@nrel.gov>
2023-04-03 12:29:48 -0600

    Add python code to collect frontal data from NOAA surface analysis archive

---
+++ surface_analysis/get_all_sfc_maps.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import os
+import numpy
+import pandas as pd
+import requests
+
+startdate = '2019-09-01'
+enddate = '2021-10-01'
+outdir = 'sfc'
+
+#analysis = 'namussfc' # CONUS
+analysis = 'usfntsfc' # fronts
+prefix = 'https://www.wpc.ncep.noaa.gov/archives/sfc'
+
+#------------------------------------------------------------------------------
+
+datetimes = pd.date_range(startdate,enddate,freq='3h')
+
+for yr in datetimes.year.unique():
+    os.makedirs(f'{outdir}/{yr}', exist_ok=True)
+
+imgurls = [f'{prefix}/{dt.year}/{analysis}{dt.strftime("%Y%m%d%H")}.gif' for dt in datetimes]
+
+for dt,url in zip(datetimes,imgurls):
+    fpath = f'{outdir}/{url[len(prefix)+1:]}'
+    if not os.path.isfile(fpath):
+        print('\rDownloading '+fpath,end='')
+        img_data = requests.get(url).content
+        with open(fpath, 'wb') as f:
+            f.write(img_data)
+print('')
